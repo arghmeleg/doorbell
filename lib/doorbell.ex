a4de7bdb0a280ad937b9afd422518afeb11e1528
Steve DeGele <arghmeleg@users.noreply.github.com>
2023-10-01 19:47:11 -0400

    get stuff working

--- lib/doorbell.ex
+++ lib/doorbell.ex
@@ -3,16 +3,145 @@ defmodule Doorbell do
   Documentation for `Doorbell`.
   """
 
-  @doc """
-  Hello world.
+  defmacro __using__(_opts \\ []) do
+    quote do
+      @on_definition {Doorbell, :on_definition}
+      @before_compile {Doorbell, :before_compile}
 
-  ## Examples
+      Module.register_attribute(__MODULE__, :gated_funs, accumulate: true)
+      Module.register_attribute(__MODULE__, :gate, accumulate: true)
+    end
+  end
 
-      iex> Doorbell.hello()
-      :world
+  def on_definition(env, :def, fun, [_conn, _params] = args, guards, body) do
+    IO.puts("------------on_definition-------------")
+    # IO.inspect(Map.keys(env), width: :infinity)
+    IO.inspect(fun)
 
-  """
-  def hello do
-    :world
+    current_gate = Module.get_attribute(env.module, :gate)
+
+    IO.inspect(current_gate)
+
+    {last_fun, last_gate} =
+      case Module.get_attribute(env.module, :gated_funs) do
+        [{_env, _def, last_fun, _args, _guards, _body, last_gate} | _] ->
+          {last_fun, last_gate}
+
+        _ ->
+          {nil, nil}
+      end
+
+    cond do
+      current_gate != [] ->
+        Module.put_attribute(
+          env.module,
+          :gated_funs,
+          {env, :def, fun, args, guards, body, current_gate}
+        )
+
+        Module.delete_attribute(env.module, :gate)
+
+      fun == last_fun ->
+        IO.puts("SAME FUNNNNNNNN")
+
+        Module.put_attribute(
+          env.module,
+          :gated_funs,
+          {env, :def, fun, args, guards, body, last_gate}
+        )
+
+      true ->
+        nil
+    end
+  end
+
+  def on_definition(_env, _kind, _fun, _args, _guards, _body), do: nil
+
+  # https://stackoverflow.com/questions/42929471/elixir-macro-rewriting-module-methods-and-global-using-macro
+  def def_clause({env, _kind, fun, args, guard, body, _gate_body}) do
+    IO.inspect(fun, label: "def clause fun")
+    under_fun = :"_#{fun}"
+
+    case guard do
+      [] ->
+        quote do
+          # Kernel.def(Macro.escape(:"_#{unquote(fun)}"), unquote(args), unquote(body))
+          # Macro.escape(:"_#{unquote(fun)}")(unquote_splicing(args), unquote(body))
+          # def(Macro.escape(:"_#{unquote(fun)}")(unquote_splicing(args), unquote(body)))
+          # def(unquote(fun)(unquote_splicing(args)), unquote(body)) # works
+          # def(Macro.escape(:"_#{fun}")(unquote_splicing(args)), unquote(body))
+          defp(unquote(under_fun)(unquote_splicing(args)), unquote(body))
+
+          # Kernel.def(:"_#{fun}", (unquote_splicing(args)), unquote(body))
+        end
+
+      _ ->
+        quote do
+          # def(
+          #   :"_#{fun}"(unquote_splicing(args)) when unquote_splicing(guard),
+          #   unquote(body)
+          # )
+        end
+    end
+  end
+
+  defmacro before_compile(env) do
+    IO.puts("~~~~~~before_compile~~~~~~~~")
+
+    [
+      :function,
+      :functions,
+      :line,
+      :module,
+      :file,
+      :context,
+      :aliases,
+      :lexical_tracker,
+      :context_modules,
+      :macro_aliases,
+      :versioned_vars,
+      :tracers,
+      :requires,
+      :macros,
+      :__struct__
+    ]
+
+    IO.inspect(env.module)
+
+    gated_funs = Module.get_attribute(env.module, :gated_funs)
+    Module.delete_attribute(env.module, :gated_funs)
+
+    grouped_gated_funs =
+      Enum.group_by(gated_funs, fn {_env, _kind, fun, _args, _guards, _body, _gate_body} ->
+        fun
+      end)
+
+    for {fun, funs} <- grouped_gated_funs do
+      {env, _kind, _fun, args, guard, body, gate_body} = funs |> Enum.reverse() |> List.first()
+      under_fun = :"_#{fun}"
+
+      {one, two, three} =
+        quote do
+          defoverridable [{unquote(fun), 2}]
+
+          # def thing(), do: :thing
+
+          def unquote(fun)(conn, params) do
+            # IO.puts(".............")
+            IO.inspect(unquote(gate_body))
+            # IO.puts("waaaaaa")
+            # unquote(body)
+            # apply(__MODULE__, :"_#{unquote(fun)}", [conn, params])
+            unquote(under_fun)(conn, params)
+          end
+        end
+        |> IO.inspect()
+
+      # |> IO.inspect(label: "pre = quoted")
+      # |> Kernel.++(Enum.map(funs, &Doorbell.def_clause/1))
+      {one, two, three ++ (funs |> Enum.reverse() |> Enum.map(&Doorbell.def_clause/1))}
+    end
+    |> IO.inspect()
+    |> Enum.reverse()
   end
 end
--- test/doorbell_test.exs
+++ test/doorbell_test.exs
@@ -2,7 +2,68 @@ defmodule DoorbellTest do
   use ExUnit.Case
   doctest Doorbell
 
+  defmodule FakeController do
+    use Doorbell
+
+    # require Ding
+
+    # @doorbell do
+    #   # param(:name, :string)
+    #   1
+    # end
+
+    # Ding.dong()
+
+    @gate do
+      :shit
+    end
+
+    def get_stuff(conn, %{"junk" => "idk"}) do
+      json(conn, %{success: true})
+    end
+
+    # @gate do
+    #   :shit
+    # end
+    def get_stuff(conn, params) do
+      IO.inspect(params)
+      json(conn, %{success: true})
+    end
+
+    # @doorbell do
+    #   2
+    # end
+    # cry do
+    #   :balls
+    # end
+
+    # @gate do
+    #   :shit
+    # end
+    def get_more(conn, params) do
+      IO.inspect(params)
+      json(conn, %{success: true})
+    end
+
+    def wtf?(v) do
+      IO.inspect(v)
+      IO.puts("ONE ONE ONE")
+    end
+
+    defp json(conn, map) do
+      Map.put(conn, :response, map)
+    end
+  end
+
   test "greets the world" do
-    assert Doorbell.hello() == :world
+    IO.puts("...................................START TEST")
+    FakeController.__info__(:functions) |> IO.inspect()
+    # IO.inspect(quote do: FakeController)
+    FakeController.get_stuff(%{}, %{"junk" => "idk"}) |> IO.inspect()
+    FakeController.get_stuff(%{}, %{"junk" => "idkw"}) |> IO.inspect()
+    # FakeController._get_stuff(%{}, %{"junk" => "idk"}) |> IO.inspect()
+    # FakeController.wtf?(3)
+    # FakeController.wtf?(1)
+    # FakeController.ding_dong() |> IO.inspect()
   end
 end
